a86207dae89e9835b2b9eea52e24f60c369a0516
Abass SARR <abasssarr0@gmail.com>
2022-12-02 18:38:31 +0000

    editing variable names for better readability

--- day2/index.js
+++ day2/index.js
@@ -7,7 +7,7 @@ async function getValue() {
     const input = fs.readFileSync('input.txt', 'utf8')
     input.split(/\n/).map(([opponent, , us]) => {
         const result = getWinnerValue(opponent, us)
-        myScore += result.totalScore
+        myScore += result.myScore
         teammateScore += result.teammateScore
     })
     console.log(`myScore: ${myScore} -- teammateScore: ${teammateScore}`)
@@ -17,7 +17,7 @@ function getWinnerValue(value1, value2) {
     /* ---------- Test 1 --------- */
     const shapeScore = { X: 1, Y: 2, Z: 3 }
     const output = { AX: 3, AY: 6, AZ: 0, BX: 0, BY: 3, BZ: 6, CX: 6, CZ: 3, CY: 0 }
-    const totalScore = output[`${value1}${value2}`] + shapeScore[value2]
+    const myScore = output[`${value1}${value2}`] + shapeScore[value2]
     /* ---------- Test 2 --------- */
     if (value2 == 'X') {
         value1 == 'A' ? (value2 = 'Z') : value1 == 'B' ? (value2 = 'X') : value1 == 'C' ? (value2 = 'Y') : (value2 = 'Y');
@@ -27,7 +27,7 @@ function getWinnerValue(value1, value2) {
         value1 == 'A' ? (value2 = 'Y') : value1 == 'B' ? (value2 = 'Z') : value1 == 'C' ? (value2 = 'X') : (value2 = 'X');
     }
     const teammateScore = output[`${value1}${value2}`] + shapeScore[value2]
-    return { totalScore, teammateScore }
+    return { myScore, teammateScore }
 }
 
 getValue()
